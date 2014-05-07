ce01bfd3918e0d9c7ff9f0aa5fbe0904870b5f62
cghersi <cghersi@snip2code.com>
2014-05-06 21:11:29 -0700

    first release

---
+++ S2CResources/Email/WelcomeEmail/PlaceHolders.cs
@@ -0,0 +1,32 @@
+﻿//------------------------------------------------------------------------------
+// (c) 2011-2014 snip2code inc.
+// This software is property of snip2code inc. Use or reproduction without permission is prohibited  
+//------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Snip2Code.Resources.Email.WelcomeEmail
+{
+    public class PlaceHolders
+    {
+        public const string UserName = "$$$Username$$$";
+        public const string FullSitePath = "$$$fullsitepath$$$";
+
+        public const string GroupName = "$$$GroupName$$$";
+        public const string GroupAdminName = "$$$GroupAdminName$$$";
+        public const string GroupDescription = "$$$GroupDescription$$$";
+
+        public const string GroupID = "$$$GroupID$$$";
+        public const string UserEmail = "$$$UserEmail$$$";
+        public const string UserUniqueToken = "$$$UserUniqueToken$$$";
+
+        public const string NewPsw = "$$$newPsw$$$";
+
+        public const string ChannelsContent = "$$$channelsContent$$$";
+
+        public const string SnippetName = "$$$SnippetName$$$";
+        public const string SnippetUrl = "$$$SnippetUrl$$$";
+    }
+}
